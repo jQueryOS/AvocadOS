48a3c6c85c5a1e7babf65b200a3dbad66ffae801
Vihan Bhargava <blue1086@gmail.com>
2016-03-19 09:03:12 -0700

    started display stuff

--- kernel/io.c
+++ kernel/io.c
@@ -1,5 +1,9 @@
 // no <stdio> :(
 
+unsigned char* row_address(unsigned char l) {
+    return (unsigned char*)0xC000 + ((l / 8) * 80) + ((l % 8) * 2048)
+}
+
 int main(int argc, char *argv[]) {
-    // some code
+    
 }
