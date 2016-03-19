02861e2313ce44b5e1ed1ddf43f051e3c086d94d
Vihan <blue1086@gmail.com>
2016-03-18 22:20:40 -0700

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,4 +1,14 @@
 ; bootloader for jqueryos
+; NOTES
+; AX - accumlator
+; BX - base (address indexing)
+; CX - counter (iteration)
+; DF - data (w/ AX and large values)
 
+	BITS 16 ; 16-bit mode
+	
 start:
 	mov ax 07C0h ; 0x07C0h is where our bootloader starts
+
+.done:
+	dw 0xAA55 ; boot signatre
