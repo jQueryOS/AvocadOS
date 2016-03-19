c64053322b8341c71485001c207d4f3fbeb6ac89
somebody1234 <somebody1234@users.noreply.github.com>
2016-03-19 19:21:46 +1000

    Changed to 64/32 bit

--- bootloader.asm
+++ bootloader.asm
@@ -5,7 +5,12 @@
 ; CX - counter (iteration)
 ; DF - data (w/ AX and large values)
 
-	BITS 16 ; 16-bit mode
+; Switches between 64-bit or 32-bit
+%ifdef X64
+	BITS 64
+%else
+	BITS 32
+%endif
 	
 start:
 	mov ax 07C0h ; 0x07C0h is where our bootloader starts
