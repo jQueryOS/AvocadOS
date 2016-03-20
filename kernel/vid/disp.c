8ac747f8437f4f367c90e64017371b69fb7c54af
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-03-19 23:57:47 -0400

    Delete disp.c

--- kernel/vid/disp.c
+++
@@ -1,6 +0,0 @@
-// NOTE: THIS DOES NOT WORK AT THE MOMENT
-#include "disp.h"
-
-unsigned char* row_address(unsigned char l) {
-    return (unsigned char*)0xC000 + ((l / 8) * 80) + ((l % 8) * 2048);
-}
